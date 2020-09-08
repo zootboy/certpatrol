163715be7642b71bc76ef42507bdbd0729fbb386
Sean Greenslade <sean@seangreenslade.com>
2020-09-07 21:04:12 -0700

    Change id string to make it clear this is a fork.

--- defaults/preferences/CertPatrol.js
+++ defaults/preferences/CertPatrol.js
@@ -8,7 +8,7 @@ pref("certpatrol.notify.timeout", 10);
 pref("certpatrol.hosts.ignore", "si0.twimg.com");
 
 // See http://kb.mozillazine.org/Localize_extension_descriptions
-pref("extensions.CertPatrol@PSYC.EU.description", "chrome://certpatrol/locale/CertPatrol.properties");
+pref("extensions.CertPatrol@zootboy.com.description", "chrome://certpatrol/locale/CertPatrol.properties");
 
 //show debug messages?
-//pref("extensions.CertPatrol@PSYC.EU.debug", false);
+//pref("extensions.CertPatrol@zootboy.com.debug", false);
